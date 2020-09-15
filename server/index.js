eb29c93cc5ba62be5202c4649eb7da85a089e8ed
ArwaLokhandwala <arwa18793@gmail.com>
2020-09-15 10:48:54 +0800

    Fullstack application

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express=require("express");
+const path=require("path");
+
+const app=express()
+
+const clientBuildPath = path.join(__dirname, '../client/build');
+console.log(clientBuildPath)
+
+app.use(express.json());
+
+app.use(express.urlencoded({
+	extended:true
+}));
+
+app.use(express.static(clientBuildPath))
+
+app.get("/",(request, response)=>{
+	response.sendFile(path.join(clientBuildPath,'/index.html'))
+})
+
+app.get("/greet",(req,res)=>{
+	res.send("Hello from server")
+})
+
+app.listen(3000, ()=>{
+	console.log("Server listening at port 3000");
+});
